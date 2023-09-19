e9e953d0a33ee8fd210e97d29d12a2a9c5ea5a4f
EliotChen8 <103273258+EliotChen8@users.noreply.github.com>
2023-09-19 20:49:36 +0800

    Update Btn.py

--- 2023_FA/Btn.py
+++ 2023_FA/Btn.py
@@ -2,8 +2,8 @@
 import RPi.GPIO as GPIO
 
 BtnPin = 11
-Rpin   = 12
-Gpin   = 13
+Gpin   = 12
+Rpin   = 13
 
 def setup():
 	GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
@@ -12,40 +12,36 @@ def setup():
 	GPIO.setup(BtnPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)    # Set BtnPin's mode is input, and pull up to high level(3.3V)
 	GPIO.add_event_detect(BtnPin, GPIO.BOTH, callback=detect, bouncetime=200)
 
-def Led(x):         #控制双色LED灯闪烁的函数
+def Led(x):
 	if x == 0:
-		GPIO.output(Rpin, 1)  #红灯亮
-		GPIO.output(Gpin, 0)  #绿灯灭
+		GPIO.output(Rpin, 1)
+		GPIO.output(Gpin, 0)
 	if x == 1:
-		GPIO.output(Rpin, 0)  #红灯灭
-		GPIO.output(Gpin, 1)  #绿灯亮
+		GPIO.output(Rpin, 0)
+		GPIO.output(Gpin, 1)
 
-def Print(x):        #打印按键是否按下的提示消息
+def Print(x):
 	if x == 0:
 		print '    ***********************'
-		print '    *   Button is down!   *'
-		print '    ***********************'
-	elif x == 1:
-		print '    ***********************'
-		print '    *   Button is up !   *'
+		print '    *   Button Pressed!   *'
 		print '    ***********************'
 
 def detect(chn):
-	Led(GPIO.input(BtnPin))    #控制双色LED灯闪烁
-	Print(GPIO.input(BtnPin))  #打印按键是否按下的提示消息
+	Led(GPIO.input(BtnPin))
+	Print(GPIO.input(BtnPin))
 
 def loop():
 	while True:
-	pass  #pass 不做任何事情，一般用做占位语句。
+		pass
 
 def destroy():
-	GPIO.output(Gpin, GPIO.LOW)       # Green led off
-	GPIO.output(Rpin, GPIO.LOW)       # Red led off
-	GPIO.cleanup()                    # Release resource
+	GPIO.output(Gpin, GPIO.HIGH)       # Green led off
+	GPIO.output(Rpin, GPIO.HIGH)       # Red led off
+	GPIO.cleanup()                     # Release resource
 
-if __name__ == '__main__':     # 主程序入口
+if __name__ == '__main__':     # Program start from here
 	setup()
 	try:
 		loop()
-	except KeyboardInterrupt:  #  'Ctrl+C' 
+	except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
 		destroy()
