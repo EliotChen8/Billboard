d05f3c49dac2b3b410eed2ace5f9b490811370f3
EliotChen8 <better197709@126.com>
2023-10-09 21:38:20 +0800

    Test1

--- 2023_FA/EE351 微机原理与微系统/Lab3/Demo.py
+++ 2023_FA/EE351 微机原理与微系统/Lab3/Demo.py
@@ -1,47 +1,29 @@
 #!/usr/bin/env python
-import RPi.GPIO as GPIO
+import PCF8591 as ADC
 
-BtnPin = 11
-Gpin   = 12
-Rpin   = 13
 
 def setup():
-	GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
-	GPIO.setup(Gpin, GPIO.OUT)     # Set Green Led Pin mode to output
-	GPIO.setup(Rpin, GPIO.OUT)     # Set Red Led Pin mode to output
-	GPIO.setup(BtnPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)    # Set BtnPin's mode is input, and pull up to high level(3.3V)
-	GPIO.add_event_detect(BtnPin, GPIO.BOTH, callback=detect, bouncetime=200)
+    ADC.setup(0x48)
 
-def Led(x):
-	if x == 0:
-		GPIO.output(Rpin, 1)
-		GPIO.output(Gpin, 0)
-	if x == 1:
-		GPIO.output(Rpin, 0)
-		GPIO.output(Gpin, 1)
-
-def Print(x):
-	if x == 0:
-		print '    ***********************'
-		print '    *   Button Pressed!   *'
-		print '    ***********************'
-
-def detect(chn):
-	Led(GPIO.input(BtnPin))
-	Print(GPIO.input(BtnPin))
 
 def loop():
-	while True:
-		pass
+    while True:
+        print
+        ADC.read(0)
+        # 打印电位计电压大小A/D转换后的数字值（从AIN0借口输入的）
+        # 范围是0~255,0时LED灯熄灭，255时灯最亮
+        ADC.write(ADC.read(0))
+    # 将0通道输入的电位计电压数字值转化成模拟值从AOUT输出
 
+
+# 给LED灯提供电源VCC输入
 def destroy():
-	GPIO.output(Gpin, GPIO.HIGH)       # Green led off
-	GPIO.output(Rpin, GPIO.HIGH)       # Red led off
-	GPIO.cleanup()                     # Release resource
+    ADC.write(0)
 
-if __name__ == '__main__':     # Program start from here
-	setup()
-	try:
-		loop()
-	except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
-		destroy()
+
+if __name__ == "__main__":
+    try:
+        setup()
+        loop()
+    except KeyboardInterrupt:
+        destroy()
